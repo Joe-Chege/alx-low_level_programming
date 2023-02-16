8ea8fae489bb09e8c8a92e92c8124a856b877bed
Joe-Chege <njorogejosephchege@gmail.com>
2023-02-16 05:36:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 /**
  * main  - print "Programming is like building a multilingual puzzle"
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
