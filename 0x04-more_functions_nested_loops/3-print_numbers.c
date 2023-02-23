ee22f26760978504a193fd373ab47a686b8c569c
Joe-Chege <njorogejosephchege@gmail.com>
2023-02-23 05:16:59 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+* print_numbers - prints the numbers 0 through 9
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+
+_putchar('\n');
+}
