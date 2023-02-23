dd83da32fa64ea5cc0437cf1d4b390da867d9855
Joe-Chege <njorogejosephchege@gmail.com>
2023-02-23 05:44:46 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - prints a stright line  n
+* @n: the length of the line printed
+*
+* Return: void
+*/
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+
+_putchar('\n');
+}
