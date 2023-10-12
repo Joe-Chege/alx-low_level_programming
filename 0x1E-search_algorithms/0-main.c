3bd9cd77c2d0523ecdb4130930c216912d13e90f
Joe-Chege <njorogejosephchege@gmail.com>
2023-10-12 00:04:47 -0700

    main.c

---
+++ 0x1E-search_algorithms/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+
+
+  return (EXIT_SUCCESS);
+}
